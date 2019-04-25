94de4d17fb4a334d6155ac3ccec70cb748fa6c94
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-04-25 22:58:59 +0530

    movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,15 +6,20 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour {
 
     [SerializeField] float xSpeed = 4f;
+    [SerializeField] float xClamp = 10f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        float xoffset = xThrow * xSpeed * Time.deltaTime;
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float xPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xPos, xClamp, -xClamp);
+
+        transform.localPosition = new Vector3(clampedXPos , transform.localPosition.y, transform.localPosition.z);
 	}
 }
