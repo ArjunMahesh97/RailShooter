3f4935d9ce821d556acc8a8dff3fcdc19937c66d
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-05-03 22:44:17 +0530

    death

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour {
+
+    [SerializeField] GameObject deathFX;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("LoadLevel", 2f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerController: MonoBehaviour {
+
+    [SerializeField] float Speed = 15f;
+    [SerializeField] float xClamp = 10f;
+    [SerializeField] float yClamp = 6f;
+
+    [SerializeField] float positionXFactor = -1.5f;
+    [SerializeField] float controlXFactor = -20f;
+    [SerializeField] float positionYFactor = 1.5f;
+    [SerializeField] float controlZFactor = -20f;
+
+    float xThrow, yThrow;
+    bool isControlEnabled = true;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isControlEnabled)
+        {
+            ProcessTranslation();
+            ProcessRotation();
+        }
+    }
+
+    void OnPlayerDeath()
+    {
+        isControlEnabled = false;
+    }
+
+
+
+    private void ProcessRotation()
+    {
+        float xRot = transform.localPosition.y * positionXFactor + yThrow * controlXFactor;
+        float yRot = transform.localPosition.x * positionYFactor;
+        float zRot = xThrow * controlZFactor;
+        transform.localRotation = Quaternion.Euler(xRot, yRot, zRot);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float xPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(xPos, -xClamp, xClamp);
+
+        float yOffset = yThrow * Speed * Time.deltaTime;
+        float yPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(yPos, -yClamp, yClamp);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
