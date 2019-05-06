6fab4a3b4710793bb4997b4bd039a017acd0e53a
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-05-06 18:18:49 +0530

    scoreboard

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour {
+
+    [SerializeField] int scorePerHit = 15;
+
+    int score;
+    Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Scorehit()
+    {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
